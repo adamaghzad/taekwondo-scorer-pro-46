140cf85d00ccd5900220bc86b73a2b2787685cfd
adamaghzad <111640047+adamaghzad@users.noreply.github.com>
2024-11-16 13:12:12 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,21 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
+
+import Display from "./pages/display";
 import Tournament from "./pages/Tournament";
 import Scoreboard from "./components/Scoreboard";
 
 const queryClient = new QueryClient();
+
+// The App component is the entry point for your application.
+
+// It uses React Router to define the routes for your application.
+
+// You can add more routes as needed.
+
+
+
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
@@ -16,9 +27,13 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/tournament" element={<Tournament />} />
-          <Route path="/scoreboard" element={<Scoreboard />} />
+          {/* deux ongel website on sime time i runder display*/}
+          <Route path="/display" element={<Display />} />
+
+          <Route path="/" element={<Tournament />} />
+          
+          
+          
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
